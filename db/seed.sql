d1664a30dd84836c5e70482e84002b3ecdadcc66
David Vargas <davidvargas95@gmail.com>
2021-03-21 16:38:37 -0700

    database created and connected

---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+    VALUES
+        ("Lumberjack Burger", 0),
+        ("Bacon Jalapeno Burger", 0),
+        ("Breakfast Burger", 0);
+
+SELECT * FROM burgers;
